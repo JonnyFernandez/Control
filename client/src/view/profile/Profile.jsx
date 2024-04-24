e857a44a6ddbcbc0f702a1c74db7b089e2142c09
JonnyFernandez <arcancode@gmail.com>
2024-04-23 21:33:17 -0300

    mejoras

--- client/src/view/profile/Profile.jsx
+++ client/src/view/profile/Profile.jsx
@@ -9,11 +9,106 @@ const Profile = () => {
         alert(data)
     }
 
+
+
     return (
         <div className={p.profile}>
             <NavAdmin />
             <div className={p.body}>
-                <Sidebar selector={selector} />
+
+                <div className={p.item1}>
+                    <Sidebar selector={selector} />
+                </div>
+
+                <div className={p.item2}>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                    <h1>Hello world</h1>
+                </div>
 
             </div>
         </div>
