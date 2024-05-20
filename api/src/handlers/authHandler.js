734e17632d663055a2e095159c804a715e2da64c
JonnyFernandez <arcancode@gmail.com>
2024-05-20 15:34:25 -0300

    correcciones en los errors1

--- api/src/handlers/authHandler.js
+++ api/src/handlers/authHandler.js
@@ -47,13 +47,13 @@ module.exports = {
   },
   verifyToken: async (req, res) => {
     const { token } = req.cookies;
-    if (!token) return res.status(401).json({ error: "Unauthorized" });
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
-      if (err) return res.status(401).json({ error: "Unauthorized" });
+      if (err) return res.status(401).json({ message: "Unauthorized" });
 
       const userFound = await User.findByPk(user.id);
-      if (!userFound) return res.status(401).json({ error: "Unauthorized" });
+      if (!userFound) return res.status(401).json({ message: "Unauthorized" });
 
       return res.json({
         id: userFound.id,
