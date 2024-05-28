4180f1e7450ccdfaf9c60296449e7fff4b6a8f51
JonnyFernandez <arcancode@gmail.com>
2024-05-28 10:48:28 -0300

    logica y css de likes funcinando

--- client/src/views/details/Details.jsx
+++ client/src/views/details/Details.jsx
@@ -17,7 +17,7 @@ const Details = () => {
     const details = useSelector(state => state.prod.details);
     const uniqueLikes = new Set(details.likes?.map(like => like.like));
     const numUniqueLikes = uniqueLikes.size > 0 ? uniqueLikes.size : '';
-    const prodComment = details.reviews?.length
+    const prodComment = details.reviews?.length || ''
     // console.log(prodComment);
 
 
@@ -99,8 +99,8 @@ const Details = () => {
                         <img src={details.image || "https://i.pinimg.com/564x/c9/36/cd/c936cdc3b4004f05bf4f5cfa0a671524.jpg"} alt="" />
                         <div className={z.code}>Cod: {details.code}</div>
                         {details.off !== 0 && <div className={z.off}>Descuento: {details.off * 100}%</div>}
-                        <div className={z.icons}>
 
+                        <div className={z.icons}>
                             <div className={z.likes}>
                                 <div ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
                                     <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
@@ -108,18 +108,21 @@ const Details = () => {
                                 <div className={z.numberLike}>{numUniqueLikes}</div>
                             </div>
 
-                            <div onClick={handleCommentInputChange}>Comentar</div>
-                            <div className={z.comment} onClick={handleCommentToggle}>
-                                <div className={z.divComent1}>
-                                    <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-chat" viewBox="0 0 16 16">
-                                            <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
-                                        </svg>
-                                    </div>
-                                    <div className={z.commentNumber}>{prodComment}</div>
+                            <div onClick={handleCommentInputChange} className={z.comment}>Comentar</div>
+                            <div className={z.addComment} onClick={handleCommentToggle}>
+
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-chat" viewBox="0 0 16 16">
+                                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105" />
+                                    </svg>
                                 </div>
+                                <div className={z.commentNumber}>{prodComment}</div>
+
                             </div>
                         </div>
+
+
+
                     </div>
                     <div className={z.right}>
                         <div className={z.infoProd}>
